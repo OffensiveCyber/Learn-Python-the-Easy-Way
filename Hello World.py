80311a83f284b2a6ceb69d8bed88a06e0fa5e0a5
OffensiveCyber <32565129+OffensiveCyber@users.noreply.github.com>
2018-03-27 16:51:56 +0530

    Update and rename Hello World.py to T1_HelloWorld.py

--- Hello World.py
+++
@@ -1,3 +0,0 @@
-#print is the statement which display the typed output on your screen. Note - Print is a function in Python 3.x
-
-print "Hello World!"
---
+++ T1_HelloWorld.py
@@ -0,0 +1,4 @@
+#In the world of python - This will bring you good luck. 
+#type of first python program, to display "Hello World!" on your command prompt/terminal/Interpreter.
+
+print "Hello World!"  #print is the statement which display the typed output on your screen. Note - Print() is a function in Python 3.x
